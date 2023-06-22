2737532d5096a623ca8770b5b859bb51ac2dc8bf
24Syleo <sylvain.beggiora@outlook.com>
2023-06-22 20:06:51 +0200

    ok essai demain

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useEffect } from 'react';
+import React from 'react';
 import Home from './page/home.js';
 import BarreDeNav from './component/Navbar.js';
 import CardBeer from './component/CardBeer.js';
@@ -7,20 +7,18 @@ import BeerController from './controller/beerController.js';
 
 
 function App() {
-
-    useEffect(
-        () => {
-            const beers = BeerController.getBeers();
-            console.log(beers);
-            return beers;
-        }
-    );
+    const beers = BeerController.getBeers();
+    console.log(beers);
 
     return ( <
-        Home navbar = { < BarreDeNav / > }
-        card = { < CardBeer / > }
-        />
-    )
-}
+            Home navbar = { < BarreDeNav / > }
+            card = {
+                beers.map(beer => < CardBeer image = { beer.image_url }
+                    name = { beer.name }
+                    tagline = { beer.tagline }
+                    />) } /
+                    >
+                );
+            }
 
-export default App;
+            export default App;
