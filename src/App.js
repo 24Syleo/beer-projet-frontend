c8933801a447ff0e542906a122adc1784a60d530
24Syleo <sylvain.beggiora@outlook.com>
2023-06-27 16:50:36 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -10,57 +10,52 @@ import AddBeer from './component/AddBeer.js';
 import CheckIng from './component/checkIng.js';
 import FigureExample from './page/features.js';
 
+
 function App() {
-
-    socket.on('connection', ()=>{
-        console.log('connecter a');
+    
+    socket.on("ajoutBeer", (arg1) => {
+        console.log("Ciao", arg1); // 1
     })
-
-    const [ingList, setIngList]=useState([]);
+    
+    const [ingList, setIngList] = useState([]);
     const [nameBeer, setNameBeer] = useState();
     const [tagBeer, setTagBeer] = useState();
     const [imgBeer, setImgBeer] = useState();
     const [ingBeer, setIngBeer] = useState([])
 
-    const listIng = async() => {
+    const listIng = async () => {
         const response = await IngController.getIngredients();
         setIngList(response);
     }
 
-    const nameChange = (evt)=>{
-        console.log(evt.target.value);
+    const nameChange = (evt) => {
         evt.preventDefault();
         setNameBeer(evt.target.value);
     }
 
-    const tagChange = (evt)=>{
+    const tagChange = (evt) => {
         setTagBeer(evt.target.value);
     }
 
-    const imgChange = (evt)=>{
+    const imgChange = (evt) => {
         setImgBeer(evt.target.value);
     }
 
-    const ingChange = (evt)=>{
-        console.log(evt.target.checked)
+    const ingChange = (evt) => {
         let ingListBeer = [...ingBeer]
-        if(evt.target.checked){
-            ingListBeer = [...ingBeer,evt.target.id];
+        if (evt.target.checked) {
+            ingListBeer = [...ingBeer, evt.target.id];
         }
-        console.log(ingListBeer);
         setIngBeer(ingListBeer);
-    } 
+    }
 
     const valider = () => {
-        console.log('ici');
         createBeer();
     }
 
     const createBeer = async () => {
-        const dataBeer = {"name":nameBeer, "tagline":tagBeer, "image_url":imgBeer,"ingredients":ingBeer};
-        console.log(dataBeer);
-        const res = await BeerController.createBeer(dataBeer);
-        console.log('creation',res);
+        const dataBeer = { "name": nameBeer, "tagline": tagBeer, "image_url": imgBeer, "ingredients": ingBeer };
+        await BeerController.createBeer(dataBeer);
     }
 
     useEffect(() => {
@@ -68,36 +63,36 @@ function App() {
     }, []);
 
     return (
-    <>
-        <BarreDeNav
-            children = { < AddBeer
-                validerBeer={valider}
-                ValueName={nameBeer}
-                ChangeName={nameChange}
-                ValueTag={tagBeer}
-                ChangeTag={tagChange}
-                ValueImg={imgBeer}
-                ChangeImg={imgChange}
-                checkIng={
-                    ingList.map((ing,i) => {
-                        return(<CheckIng
-                            key={i}
-                            idIng={ing._id}
-                            Name={ing.name}
-                            ChangeIng={ingChange}
+        <>
+            <BarreDeNav
+                children={< AddBeer
+                    validerBeer={valider}
+                    ValueName={nameBeer}
+                    ChangeName={nameChange}
+                    ValueTag={tagBeer}
+                    ChangeTag={tagChange}
+                    ValueImg={imgBeer}
+                    ChangeImg={imgChange}
+                    checkIng={
+                        ingList.map((ing, i) => {
+                            return (<CheckIng
+                                key={i}
+                                idIng={ing._id}
+                                Name={ing.name}
+                                ChangeIng={ingChange}
                             />)
                         })
                     }
                 />}
-         />
-    <BrowserRouter>
-        <Routes>
-            <Route index element={<Home/>}/>
-            <Route path='home' element={<Home/>}/>
-            <Route path="features" element={<FigureExample/>}/>
-        </Routes>
-    </BrowserRouter>
-    </>
+            />
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path='home' element={<Home />} />
+                    <Route path="features" element={<FigureExample />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
 export default App;
